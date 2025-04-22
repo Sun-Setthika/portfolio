08c0db320ed1da2d8ae10c8d3eb8dc1a0ea4fd4d
Sun-Setthika <sunsetthika@gmail.com>
2025-04-22 14:15:02 +0200

    modify the experience succesfully

--- script.js
+++ script.js
@@ -6,67 +6,54 @@ document.addEventListener('DOMContentLoaded', () => {
     // Add Experience Data
     const experience = [
         {
-            title: "AI Engineer",
-            company: "Cosywee",
-            dates: "2024 - Present",
+            title: "Mission and Research Development",
+            company: "Préfecture du Gard",
+            dates: "Jan 2025 - Present",
             description: `
-            <ul class="experience-list">
-                <li>Currently curating and expanding datasets to improve Optical Character Recognition (OCR) performance.</li>
-                <li>Actively managing and maintaining an OCR model in pre-production, implementing enhancements as needed.</li>
-                <li>Developing a Chatbot using Agentic RAG to provide dynamic product pricing based on size and additional items, along with general company information.</li>
-            </ul>
+                <ul class="experience-list">
+                    <li>Designed and implemented an intelligent email classification system using LLaMA 3.1 8B and UnSloth, enabling zero-shot intent recognition without labeled training data.</li>
+                    <li>Refined prompt engineering techniques to ensure the AI system responded and classified accurately and naturally according to the task, making interactions smoother for prefecutre.</li>
+                    <li>Developed a robust Retrieval-Augmented Generation (RAG) pipeline to dynamically fetch and incorporate relevant law code for response generation and letter generation task.</li>
+                </ul>
             `,
         },
         {
-            title: "AI Engineer Intern",
-            company: "Cosywee",
-            dates: "2024 - 2024",
+            title: "AI Developer on OCR Application",
+            company: "Ministry of Posts and Telecommunication, Cambodia",
+            dates: "Feb 2024 - May 2024",
             description: `
-            <ul class="experience-list">
-                <li>Constructed a comprehensive company dataset and ground truth labels to support OCR model fine-tuning.</li>
-                <li>Fine-Tuned a Pre-trained Model:
-                    <ul class="sublist">
-                        <li>Optimized a pre-trained TrOCR (Transformer-based OCR model) for general text, numeric, and special character recognition tasks.</li>
-                        <li>Adapted the model specifically to the company's proprietary dataset for enhanced accuracy.</li>
-                    </ul>
-                </li>
-                <li>Pre-trained and Fine-Tuned a Custom OCR Model:
-                    <ul class="sublist">
-                        <li>Developed a CNN-based ResNet-BiLSTM architecture for numeric recognition tasks.</li>
-                        <li>Pre-trained the model using a synthetic and open-source dataset containing ~19 million samples.</li>
-                        <li>Fine-tuned the model on a proprietary dataset of ~20,000 samples to improve performance and robustness.</li>
-                    </ul>
-                </li>
-                <li>Developed an automation pipeline to connect to a mail server using Microsoft Graph API and locally download mailboxes containing quotations or purchase orders for OCR.</li>
-            </ul>
-
-
+                <ul class="experience-list">
+                    <li>Developed OCR system (Optical Character Recognition) tailored for Khmer scripts—addressing the nation's unique linguistic and technical needs. The goal was to digitize printed Khmer text—making it accessible, searchable, and editable.This involved recognizing five specific fonts commonly used in official and historical texts: Khmer Mondulkiri, Battambang, Khmer OS Siemreap, Khmer OS Muol Light, and Khmer OS Bokor.</li>
+                    <li>Construct synthetic datasets from raw text files using PyTesseract and OpenCV, enhancing training diversity in font size, fonts to model robustness.</li>
+                    <li>Fine-tuned CNN-GRU models to accurately recognize complex character structures and extract text from the input image, improving digital accessibility in Cambodian public services.</li>
+                    <li>Built a user-friendly interface with Gradio that allowed non-technical users to upload image and extract, edit, and manage text from images effortlessly.</li>
+                </ul>
             `,
-            // description: "Pre-trained and Fine-tuned a Custom OCR Model"
         },
         {
-            title: "Research and Development Mission",
-            company: "IMT Mines Ales",
-            dates: "2024 - 2024",
+            title: "Full-stack Developer",
+            company: "Institute of Technology of Cambodia",
+            dates: "Jul 2023 - Sep 2023",
             description: `
-            <ul class="experience-list">
-                <li>Build and compare a regression model to forecast the level of glucose in 30 minutes using a hybrid CNN-LSTM model.</li>
-                <li>Build a classification model to classify a patient's state (hyperglycemia, hypoglycemia, normal) based on the recorded data using the same model.</li>
-            </ul>
+                <ul class="experience-list">
+                    <li>Build and launched an online application system for Master's candidates to cutdown on paper work, streamlining admissions for thousands of students.</li>
+                    <li>Developed role-based workflows using Laravel and Vue.js for applicants, program heads, and administrators.</li>
+                    <li>Enabled features such as document uploads, draft-saving, live application tracking, and automated PDF/Excel report generation—modernizing the educational infrastructure.</li>
+                </ul>
             `,
-            // description: "Pre-trained and Fine-tuned a Custom OCR Model"
         },
         {
-            title: "Intern",
-            company: "GEOCHANVRE",
-            dates: "2024 - 2024",
+            title: "POS System Developer",
+            company: "Institute of Technology of Cambodia",
+            dates: "May 2023 - Jul 2023",
             description: `
-            <ul class="experience-list">
-                <li>Develop an inventory management application and database for the factory's stock control.</li>
-                <li>Integrate QR codes for each product type into the application.</li>
-            </ul>
+                <ul class="experience-list">
+                    <li>Created a fully operational Point-of-Sale (POS) system tailored to café environments using Java and Spring Boot and Thymleaf.</li>
+                    <li>Implemented inventory management, operations by managing orders, tables reservation, billing, and staff roles. </li>
+                    <li>Key features included real-time order tracking, digital receipts generation, menu management, and role-based access for cashiers and administrators. The backend was built with Spring Boot to ensure scalability, modularity, and efficient API handling. The application aimed to improve service speed, reduce human error, and provide insightful sales reports for business analysis.</li>
+                   
+                </ul>
             `,
-            // description: "Pre-trained and Fine-tuned a Custom OCR Model"
         }
     ];
 
@@ -76,20 +63,20 @@ document.addEventListener('DOMContentLoaded', () => {
             title: "AI Agents Fundamentals",
             issuer: "Hugging Face",
             date: "2025",
-            link: "https://cdn-lfs-us-1.hf.co/repos/f2/34/f2344151f60f6027c436821dc61cf3f27a46435de57df8df50ad02b5acca7c07/989d779a38da66c709cb92d6319a9ba6e7188181241630a2de297d86d1a0e363?response-content-disposition=inline%3B+filename*%3DUTF-8%27%272025-02-21.png%3B+filename%3D%222025-02-21.png%22%3B&response-content-type=image%2Fpng&Expires=1742732019&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTc0MjczMjAxOX19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLWxmcy11cy0xLmhmLmNvL3JlcG9zL2YyLzM0L2YyMzQ0MTUxZjYwZjYwMjdjNDM2ODIxZGM2MWNmM2YyN2E0NjQzNWRlNTdkZjhkZjUwYWQwMmI1YWNjYTdjMDcvOTg5ZDc3OWEzOGRhNjZjNzA5Y2I5MmQ2MzE5YTliYTZlNzE4ODE4MTI0MTYzMGEyZGUyOTdkODZkMWEwZTM2Mz9yZXNwb25zZS1jb250ZW50LWRpc3Bvc2l0aW9uPSomcmVzcG9uc2UtY29udGVudC10eXBlPSoifV19&Signature=rDun1BvPyPu2QajZkkZrPfXh%7E17HHF%7E6fkYLjxszIt4jZRJTlO5Wlji9pxcGaCiBJkyOw7E2Eabfgp-uaaRVPbTcxrQe0fNTgPEl6BmkMdNMzMrO19SrCGKLz-MW1xCHP9s1w9oQqokThFqlvHQ0T6lpBkw%7Eb6JX7BYyVE4KyNVwNOKof%7EuaHX3EgPcJHjGkExehncrpWujm-130lkHhekOW7d3lKW38dCKud%7Eh6-GoBe8vev9ASzi2yU2tNvf%7E13MrUbpyYlu5pbJycW5XUGCNOUJu%7E4d4p9JrEIJQMEA6YzKPyP%7E03VKymeOJX-DHJmZS3Li1Q0hrwXKhYUjt9wQ__&Key-Pair-Id=K24J24Z295AEI9"
+            link: "https://shorturl.at/ZP4EE"
         },
         {
-            title: "Advanced Learning Algorithms",
-            issuer: "Coursera",
-            date: "2024",
-            link: "https://www.coursera.org/account/accomplishments/verify/8ALUPZRDWUNY"
+            title: "Deep RL Course",
+            issuer: "Hugging Face",
+            date: "2025",
+            link: "https://huggingface.co/learn/deep-rl-course/en/communication/certification"
         },
-        {
-            title: "ML: Regression and Classification",
-            issuer: "Coursera",
-            date: "2024",
-            link: "https://www.coursera.org/account/accomplishments/verify/WKVBV7H58XPA"
-        }
+        // {
+        //     title: "ML: Regression and Classification",
+        //     issuer: "Coursera",
+        //     date: "2024",
+        //     link: "https://www.coursera.org/account/accomplishments/verify/WKVBV7H58XPA"
+        // }
     ];
 
     // Populate Experience
@@ -178,11 +165,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Dynamic Content
     const skills = [
-        'Python', 'TensorFlow', 'PyTorch', 'FastAPI', 'Flask', 'JavaScript', 'PHP', 'MLops',
-        'Laravel', 'Streamlit', 'Shell Scripting', 'Vue.js', 'Spring Boot', 'HTML', 'CSS', 'Tailwind CSS', 'Docker', 'Google Cloud Platform',
-        'NLP','Computer Vision' ,'Deep Learning', 'Deep Reinforcement Learning', 'LLM', 'Linux',
-        'Git', 'Machine Learning', 'PERT', 'OCR', 'Object Detection', 'Object Classification',
-        'Web Development',
+        'Python', 'TensorFlow', 'PyTorch', 'Gradio', 'Streamlit', 'PHP', 
+        'Laravel', 'Vue.js', 'Spring Boot', 'HTML', 'CSS', 'Tailwind CSS', 'Docker', 'JavaScript', 'C++',
+        'NLP','LLM','Computer Vision' ,'Deep Learning', 'Deep Reinforcement Learning',
+        'Machine Learning','OCR (Optical Character Recognition)','Git', 'Linux', 'Web Development',
+        
     ];
 
     const projects = [
@@ -206,26 +193,26 @@ document.addEventListener('DOMContentLoaded', () => {
             tech: 'Python, LLM, PyTorch, Deep Learning, Fine Tuning, LangChain, PERT, Git'
         },
         {
-            id: 'handmotion-prediction',
-            title: 'HandMotion Prediction',
+            id: 'Finetune Text to Speech',
+            title: 'Finetune Text to Speech',
             description: 'Developing a machine learning project to predict the acceleration of patients hands, providing one prediction per second for each hand from 7 AM to 7 PM. The dataset contains acceleration values (x, y, z) for both hands and corresponding timestamps, with 50 data points recorded per second. Responsibilities include synchronizing datasets for both hands by aligning timestamps, handling missing data through linear interpolation, and training a Long Short-Term Memory (LSTM) model to produce accurate, second-level predictions.',
             tech: 'Python, Data Analysis, PyTorch, Time-series, Git'
         },
         {
-            id: 'object-detection',
-            title: 'Application of the LoRA on Object Detection model',
+            id: 'Data Visualization',
+            title: 'Data Visualization',
             description: `
                 <ul>
-                    <li>This research project was conducted in collaboration with a PhD student from Sorbonne University. The objective was to explore the application of LoRA (Low-Rank Adaptation) to enhance the performance of DiffusionDet, an object detection model, under few-shot learning conditions. The goal was to evaluate whether LoRA can effectively adapt the model to new object categories using limited annotated data, improving efficiency and generalization in low-resource scenarios.</li>
-                    <li>We applied this approach to aerial imagery datasets, specifically DOTA (Dataset for Object Detection in Aerial Images) and DIOR (Dataset for Object Recognition in Aerial Images). These datasets contain complex scenes with multiple object classes captured from aerial perspectives, making them ideal for evaluating few-shot object detection in real-world scenarios.
+                    <li>Designed and implemented a data analysis pipeline for social media interaction patterns, processing raw survey data through comprehensive cleaning, feature engineering, and categorical encoding using R, dplyr, and tidy</li>
+                    <li>Developed and deployed interactive dashboard visualizations (Shiny) to explore relationships between time spent on platforms, stranger interactions, and psychological impacts </li>
                     </li>
                 </ul>
             `,
-            tech: 'Python, PyTorch, LoRA, Git'
+            tech: 'R, Shinyapp, Python, Data Analysis, Git'
         },
         {
-            id: 'object-classification',
-            title: 'Object classification',
+            id: 'Movie Review Sentiment Classification: A Comparative Study of Traditional and Deep Learning Models',
+            title: 'Movie Review Sentiment Classification: A Comparative Study of Traditional and Deep Learning Models',
             description: `Trained a MobileViT model on the SPOTS-10: Animal Pattern dataset to classify animals based on their body patterns and textures, and on the CIFAR-10 dataset to classify objects. Developed an API using FastAPI, deployed the model trained on CIFAR-10 with Docker on Google Cloud Platform, and created a user interface hosted on Streamlit Community Cloud.`,
             tech: 'Python, PyTorch, Git, Google Cloud Platform, FastAPI, Docker, Streamlit'
         },
@@ -233,14 +220,14 @@ document.addEventListener('DOMContentLoaded', () => {
             id: 'e-commerce',
             title: 'E-Commerce Web application',
             description: `Developed a web application for sports shoe sales using a Vue.js frontend and a Laravel backend. The frontend, built with Vue.js, provided a dynamic and responsive user interface, enabling users to browse products, filter by size, brand, and category, and manage their shopping cart in real time. The backend, powered by Laravel, handled key functionalities such as user authentication, product management, inventory tracking, order processing, and payment integration. The system also included an admin dashboard for managing product listings, viewing sales analytics, and handling customer queries. The application aimed to deliver a seamless e-commerce experience optimized for both desktop and mobile users.`,
-            tech: 'Java script, PHP, Vue.js, Laravel, HTML, CSS, Tailwind, Git'
+            tech: ' PHP, Vue.js, Laravel, HTML, CSS, Javascript, Tailwind, Git'
         },
-        {
-            id: 'cafe-system',
-            title: 'Café System',
-            description: `Developed a Point of Sale (POS) management system for a café using Java and the Spring Boot backend framework. The system was designed to streamline daily operations by managing orders, inventory, tables, billing, and staff roles. Key features included real-time order tracking, digital receipts, menu management, and role-based access for cashiers and administrators. The backend was built with Spring Boot to ensure scalability, modularity, and efficient API handling. The application aimed to improve service speed, reduce human error, and provide insightful sales reports for business analysis.`,
-            tech: 'Java, Spring Boot, HTML, CSS, Tailwind, Java script ,Git'
-        },
+        // {
+        //     id: 'cafe-system',
+        //     title: 'Café System',
+        //     description: `Developed a Point of Sale (POS) management system for a café using Java and the Spring Boot backend framework. The system was designed to streamline daily operations by managing orders, inventory, tables, billing, and staff roles. Key features included real-time order tracking, digital receipts, menu management, and role-based access for cashiers and administrators. The backend was built with Spring Boot to ensure scalability, modularity, and efficient API handling. The application aimed to improve service speed, reduce human error, and provide insightful sales reports for business analysis.`,
+        //     tech: 'Java, Spring Boot, Thymeleaf, Tailwind, Java script ,Git'
+        // },
         {
             id: 'air-writing',
             title: 'Air Writing',
@@ -319,6 +306,7 @@ const education = [
         institution: "Institute of Technology of Cambodia",
         dates: "2019 - 2024",
         details: "",
+        certificateLink:"https://drive.google.com/file/d/1_l47ltz4hGzPOs0fvZl3AL-kYQWtIssW/view?usp=drive_link" 
         // link: "https://www.itc.edu.kh", // Add the URL you want to link to
         // linkText: "View Institution" // Optional: Custom link text
     }
@@ -329,6 +317,13 @@ const educationTimeline = document.querySelector('.education-timeline');
 education.forEach(edu => {
     const div = document.createElement('div');
     div.className = 'education-item';
+    // const certificateLink = edu.certificateLink 
+    //     ? `<div class="education-certificate">
+    //          <a href="${edu.certificateLink}" target="_blank" class="certificate-link">
+    //            View Certificate
+    //          </a>
+    //        </div>`
+    //     : '';
     div.innerHTML = `
         <h3>${edu.degree}</h3>
         <div class="education-institution">${edu.institution}</div>
